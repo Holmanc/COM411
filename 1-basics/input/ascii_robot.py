19672d31bee315b4a2ce4ff22ec1903cdfc02d71
repl.it user <replituser@example.com>
2020-10-05 17:02:05 +0000

    ASCII robot with dynamic eyes

---
+++ 1-basics/input/ascii_robot.py
@@ -0,0 +1,8 @@
+#Please enter a character for the eye
+print("Enter a character for the eye")
+name = input()
+#Beep's expression is now as follows:
+print("##########")
+print("#" ,name, "#")
+print("#   xxx  #")
+print("##########")
